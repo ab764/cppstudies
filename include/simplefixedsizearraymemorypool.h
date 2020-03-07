6889ec3f8f9ad9472154ed1489439e73aa066071
ab764 <tony.basa@gmail.com>
2020-03-07 21:14:22 +0900

    Update simplefixedsizearraymemorypool.h

--- include/simplefixedsizearraymemorypool.h
+++ include/simplefixedsizearraymemorypool.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstdint>
-#include <iostream>
 
 namespace ab764 {
 
@@ -20,38 +19,30 @@ private:
 public:
   FixedSizeArrayMemoryPool() : freeListHead_(nullptr) {
 
-    std::cout << "sizeof(Node): " << sizeof(Node) << std::endl;
-
     Node* p = reinterpret_cast<Node*>(storage_);
     freeListHead_ = p;
-    std::cout << "freeListHead_: " << freeListHead_ << std::endl;
     for (int i = 0; i < SIZE-1; ++i, ++p) {
-      std::cout << "setting, p->next_ = p+1: " << p << std::endl;
       p->next_ = (p+1); 
     }
-    std::cout << "finally, p->next_ = nullptr: " << p << std::endl;
     p->next_ = nullptr;
 
   }
 
   ~FixedSizeArrayMemoryPool() {}
 
-  T* alloc() {
+  template <typename... Args> T* alloc(Args &&... args) {
     if (freeListHead_ == nullptr) return nullptr;
     T* p = reinterpret_cast<T*>(freeListHead_);
     freeListHead_ = freeListHead_->next_;
-    std::cout << "alloc(): " << p << std::endl;
+    new (p) T(std::forward<Args>(args)...);
     return p;
   }
 
   void free(T* p) {
-    if (p!=nullptr) {
-      // p->~T();
-      Node* pnode = reinterpret_cast<Node*>(p);
-      pnode->next_ = freeListHead_;
-      freeListHead_ = pnode;
-      std::cout << "free() freeListHead_: " << freeListHead_ << std::endl;
-    }
+    p->T::~T();
+    Node* pnode = reinterpret_cast<Node*>(p);
+    pnode->next_ = freeListHead_;
+    freeListHead_ = pnode;
   }
 };
 
