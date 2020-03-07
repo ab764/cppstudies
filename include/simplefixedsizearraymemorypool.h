b4ee40d9986bdeaab28058b9d6cc825ec289717d
ab764 <tony.basa@gmail.com>
2020-03-07 21:25:50 +0900

    Update simplefixedsizearraymemorypool.h

--- include/simplefixedsizearraymemorypool.h
+++ include/simplefixedsizearraymemorypool.h
@@ -20,7 +20,7 @@ public:
   FixedSizeArrayMemoryPool() : freeListHead_(nullptr), storage_(nullptr) {
 
     storage_ = (Node*)::malloc(sizeof(T) * SIZE);
-    Node* p = reinterpret_cast<Node*>(storage_);
+    Node* p = storage_;
     freeListHead_ = p;
     for (int i = 0; i < SIZE-1; ++i, ++p) {
       p->next_ = (p+1); 
@@ -33,7 +33,7 @@ public:
 
   template <typename... Args> T* alloc(Args &&... args) {
     if (freeListHead_ == nullptr) return nullptr;
-    T* p = reinterpret_cast<T*>(freeListHead_);
+    T* p = &freeListHead_->data_;
     freeListHead_ = freeListHead_->next_;
 
     new (p) T(std::forward<Args>(args)...);
