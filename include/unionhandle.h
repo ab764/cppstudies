8801068b4d7f04e9e6a32d06a645059231b68178
ab764 <tony.basa@gmail.com>
2020-03-07 18:15:13 +0900

    Create unionhandle.h

---
+++ include/unionhandle.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cstdint>
+
+namespace ab764 {
+
+template <typename TID, typename TC, typename TI>
+union Handle {
+
+  using IDType = TID;
+  using CountType = TC;
+  using IndexType = TI;
+
+  IDType id_;
+  
+  struct {
+    IndexType index_;
+    CountType count_;
+  };
+
+  void set(CountType count, IndexType index)
+  {
+    count_ = count;
+    index_ = index;
+  }
+
+  IDType id() const { return id_; }
+  CountType count() const { return count_; }
+  IndexType index() const { return index_; }
+
+  operator std::size_t() { return index_; }
+
+  void incrementCount() { ++count_; }
+};
+
+}
