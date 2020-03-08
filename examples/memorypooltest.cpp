45f1cd15a58c271c097947f3b9ad6b2bc6ef0304
ab764 <tony.basa@gmail.com>
2020-03-08 20:23:09 +0900

    Create memorypooltest.cpp

---
+++ examples/memorypooltest.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+#include "nomemorypool.h"
+#include "fixedsizeelementmemorypool.h"
+#include "inelasticfixedsizememorypool.h"
+#include "simplefixedsizearraymemorypool.h"
+
+namespace mempooltest {
+
+struct S {
+  char buff[64];
+};
+
+const int ALLOCCOUNT = 64 * 1024;
+S* arr[ALLOCCOUNT];
+const int PAGESIZE = 1024 * 16;
+
+template <typename T>
+void memoryPoolTest(T& pool)
+{
+  int count = 0;
+  for (int i = 0; i < 1024; ++i) {
+    for (int i=0; i<ALLOCCOUNT; ++i) {
+      arr[i] = pool.alloc();
+    }
+
+    for (int i=0; i<ALLOCCOUNT; ++i) {
+      pool.free(arr[i]);
+    }
+
+    count += ALLOCCOUNT;
+  }
+  std::cout << "Totall allocations/free: " << count << std::endl;
+}
+
+void testNoMemoryPool()
+{
+  ab764::NoMemoryPool<S> pool;
+  memoryPoolTest<ab764::NoMemoryPool<S>>(pool);
+}
+
+void testFixedSizeElementMemoryPool()
+{
+  ab764::FixedSizeElementMemoryPool<S, PAGESIZE> pool;
+  memoryPoolTest<ab764::FixedSizeElementMemoryPool<S, PAGESIZE>>(pool);
+}
+
+void testInelasticFixedSizeMemoryPool()
+{
+  ab764::InelasticFixedSizeMemoryPool<S, PAGESIZE> pool;
+  memoryPoolTest<ab764::InelasticFixedSizeMemoryPool<S, PAGESIZE>>(pool);
+}
+
+void testSimpleFixedSizeArrayMemoryPool()
+{
+  ab764::SimpleFixedSizeArrayMemoryPool<S, ALLOCCOUNT> pool;
+  memoryPoolTest<ab764::SimpleFixedSizeArrayMemoryPool<S, ALLOCCOUNT>>(pool);
+}
+
+}
+
+int main(const int argc, const char **argv)
+{
+  std::cout << "start" << std::endl;
+
+  const int which = ::atoi(argv[1]);
+  switch (which) {
+  case 1: mempooltest::testNoMemoryPool(); break;
+  case 2: mempooltest::testFixedSizeElementMemoryPool(); break;
+  case 3: mempooltest::testInelasticFixedSizeMemoryPool(); break;
+  case 4: mempooltest::testSimpleFixedSizeArrayMemoryPool(); break;
+  }
+  std::cout << "finished" << std::endl;
+
+  return 0;
+}
