6a4142083e5bb9f2ba9e9d3993e5d9bec398303f
ab764 <tony.basa@gmail.com>
2020-03-08 16:35:26 +0900

    Create fixedsizeelementmemorypool.h

---
+++ include/fixedsizeelementmemorypool.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <cstdint>
+
+namespace ab764 {
+
+template <typename T, int PAGESIZE>
+class FixedSizeElementMemoryPool {
+private:
+
+  union Node {
+    Node*  next_;
+    T      data_;
+  };
+
+  struct Page {
+    Page* nextPage_;
+    Node* storage_;
+    Node* freeListHead_;
+
+    Page() : nextPage_(nullptr), storage_(nullptr) {
+      freeListHead_ = storage_ = (Node*)::malloc(sizeof(Node) * PAGESIZE);
+      Node* p = storage_;
+      for (int i = 0; i < PAGESIZE-1; ++i, ++p) {
+        p->next_ = (p+1); 
+      }
+      p->next_ = nullptr;
+    }
+
+    ~Page() { ::free(storage_); }
+
+    template <typename... Args> T* alloc(Args &&... args) {
+      assert(freeListHead_ != nullptr);
+   
+      T* p = &freeListHead_->data_;
+      freeListHead_ = freeListHead_->next_;
+
+      new (p) T(std::forward<Args>(args)...);
+      return p;
+    }
+
+    bool free(T* p) {
+
+      Node* pnode = reinterpret_cast<Node*>(p);
+
+      if (pnode < storage_ || pnode > (storage_ + PAGESIZE)) return false;
+
+      p->T::~T();
+      pnode->next_ = freeListHead_;
+      return true;
+    }
+
+    bool hasFreeSlot() const { return freeListHead_ != nullptr; }
+  };
+
+  Page* pageHead_;
+
+public:
+
+  FixedSizeElementMemoryPool() {
+    pageHead_ = new Page();
+  }
+
+  ~FixedSizeElementMemoryPool() {
+    while (pageHead_ != nullptr) {
+      Page* p = pageHead_;
+      pageHead_ = pageHead_->nextPage_;
+      delete p;
+    }
+  }
+
+  template <typename... Args> T* alloc(Args &&... args) {
+    Page* page = pageHead_;
+    while (page!=nullptr && !page->hasFreeSlot()) { page = page->nextPage_; }
+
+    if (page==nullptr) {
+      page = new Page();
+      page->nextPage_ = pageHead_;
+      pageHead_ = page;
+    }
+
+    return page->alloc(std::forward<Args>(args)...);
+  }
+
+  void free(T* p) {
+    Page* page = pageHead_;
+    while (page!=nullptr && !page->free(p)) { page = page->nextPage_; }
+  }
+};
+
+}
