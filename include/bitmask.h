17334e05a87560ed5c699dd351abe9df2db4816b
ab764 <28755597+ab764@users.noreply.github.com>
2020-03-12 02:19:34 +0900

    Update bitmask.h

--- include/bitmask.h
+++ include/bitmask.h
@@ -6,18 +6,18 @@
 namespace ab1764 {
 
 template <int S>
-struct bitmask {
+struct mybitset {
 
   static const uint64_t SIZE = S;
   static const uint64_t SHIFTBITS = 6;
   static const uint64_t BITSPERELEMENT = 64;
-  static const uint64_t ARRAYSIZE = SIZE >> SHIFTBITS;
+  static const uint64_t ARRAYSIZE = ((SIZE+15) >> SHIFTBITS);
   static const uint64_t LASTBIT = (SIZE-1);
   static const uint64_t MSB64 = 0x8000000000000000;
 
   uint64_t flags_[ARRAYSIZE];
 
-  bitmask() { ::memset(flags_, 0, sizeof(flags_)); }
+  mybitset() { clearAll(); }
 
   static constexpr size_t toIndex(const size_t bit) { return bit >> SHIFTBITS; }
   static constexpr size_t toOffset(const size_t bit) { return bit & ((1 << SHIFTBITS) - 1); }
@@ -26,15 +26,18 @@ struct bitmask {
   bool isClear(size_t bit) const { return !isSet(bit); }
 
   void set(const size_t bit) { flags_[toIndex(bit)] |= (MSB64 >> toOffset(bit)); }
+  void setAll() { ::memset(flags_, 0xff, sizeof(flags_)); }
   void clear(const size_t bit) { flags_[toIndex(bit)] &= ~(MSB64 >> toOffset(bit)); }
+  void clearAll() { ::memset(flags_, 0, sizeof(flags_)); }
 
   size_t findFirstClearBit() const {
-    for (size_t i = 0; i < ARRAYSIZE; ++i) {
-      const size_t flags = flags_[i]; 
-      if (flags != ~0) {
+    const auto end = flags_ + ARRAYSIZE;
+    for (const uint64_t* flags = flags_; flags < end; ++flags) {
+      if (*flags != ~0) {
         uint64_t bit = MSB64;
         for (size_t j = 0; j < BITSPERELEMENT; ++j) {
-          if (!(flags & (bit >>= 1)) return (i << SHIFTBITS) + j;
+          if (!(*flags & bit)) return ((flags - flags_) << SHIFTBITS) + j;
+          bit >>= 1;
         }
       }
     }
